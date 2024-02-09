e31d9d617a185cc452e5608a4314bd31d7a6668b
Altashfirs <91253069+Altashfirs@users.noreply.github.com>
2024-02-09 23:55:42 +0700

    Add files via upload

--- 123230062_Akbar.cpp
+++ 123230062_Akbar.cpp
@@ -16,36 +16,36 @@ void matriksPertambahan() {
   system("cls");
   int matA[10][10], matB[10][10], hasil[10][10], maxKolom, maxBaris;
 
-  cout << "Masukkan jumlah kolom Matriks A dan B : ";
-  cin >> maxKolom;
   cout << "Masukkan jumlah baris Matriks A dan B : ";
   cin >> maxBaris;
+  cout << "Masukkan jumlah kolom Matriks A dan B : ";
+  cin >> maxKolom;
 
   cout << "\nMatriks A" << endl;
-  for (int i = 0; i < maxKolom; i++) {
-    for (int j = 0; j < maxBaris; j++) {
+  for (int i = 0; i < maxBaris; i++) {
+    for (int j = 0; j < maxKolom; j++) {
       cout << "[" << i << "][" << j << "] : ";
       cin >> matA[i][j];
     }
   }
 
   cout << "\nMatriks B" << endl;
-  for (int i = 0; i < maxKolom; i++) {
-    for (int j = 0; j < maxBaris; j++) {
+  for (int i = 0; i < maxBaris; i++) {
+    for (int j = 0; j < maxKolom; j++) {
       cout << "[" << i << "][" << j << "] : ";
       cin >> matB[i][j];
     }
   }
 
-  for (int i = 0; i < maxKolom; i++) {
-    for (int j = 0; j < maxBaris; j++) {
+  for (int i = 0; i < maxBaris; i++) {
+    for (int j = 0; j < maxKolom; j++) {
       hasil[i][j] = matA[i][j] + matB[i][j];
     }
   }
 
   cout << "\nHasil : " << endl;
-  for (int i = 0; i < maxKolom; i++) {
-    for (int j = 0; j < maxBaris; j++) {
+  for (int i = 0; i < maxBaris; i++) {
+    for (int j = 0; j < maxKolom; j++) {
       cout << "|" << setw(3) << hasil[i][j] << "" << setw(3);
     }
     cout << " | " << endl;
@@ -57,36 +57,36 @@ void matriksPengurangan() {
   system("cls");
   int matA[10][10], matB[10][10], hasil[10][10], maxKolom, maxBaris;
 
-  cout << "Masukkan jumlah kolom Matriks A dan B : ";
-  cin >> maxKolom;
   cout << "Masukkan jumlah baris Matriks A dan B : ";
   cin >> maxBaris;
+  cout << "Masukkan jumlah kolom Matriks A dan B : ";
+  cin >> maxKolom;
 
   cout << "\nMatriks A" << endl;
-  for (int i = 0; i < maxKolom; i++) {
-    for (int j = 0; j < maxBaris; j++) {
+  for (int i = 0; i < maxBaris; i++) {
+    for (int j = 0; j < maxKolom; j++) {
       cout << "[" << i << "][" << j << "] : ";
       cin >> matA[i][j];
     }
   }
 
   cout << "\nMatriks B" << endl;
-  for (int i = 0; i < maxKolom; i++) {
-    for (int j = 0; j < maxBaris; j++) {
+  for (int i = 0; i < maxBaris; i++) {
+    for (int j = 0; j < maxKolom; j++) {
       cout << "[" << i << "][" << j << "] : ";
       cin >> matB[i][j];
     }
   }
 
-  for (int i = 0; i < maxKolom; i++) {
-    for (int j = 0; j < maxBaris; j++) {
+  for (int i = 0; i < maxBaris; i++) {
+    for (int j = 0; j < maxKolom; j++) {
       hasil[i][j] = matA[i][j] - matB[i][j];
     }
   }
 
   cout << "\nHasil : " << endl;
-  for (int i = 0; i < maxKolom; i++) {
-    for (int j = 0; j < maxBaris; j++) {
+  for (int i = 0; i < maxBaris; i++) {
+    for (int j = 0; j < maxKolom; j++) {
       cout << "|" << setw(3) << hasil[i][j] << "" << setw(3);
     }
     cout << " | " << endl;
